1b406de8599be36907c68a97c11187d3fe5f4783
kmizmal <kmizzmal@gmail.com>
2025-03-17 19:01:44 +0800

    更新配置文件，修改社交链接和网站链接，调整壁纸加载逻辑，优化背景组件，更新更多内容展示

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -32,11 +32,6 @@ const bgUrl = ref(null);
 const imgTimeout = ref(null);
 const emit = defineEmits(["loadComplete"]);
 
-// 壁纸随机数
-// 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
-// const bgRandom = Math.floor(Math.random() * 10 + 1);
-
-
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
